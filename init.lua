1971f24354099243e6272ced205ac395877f4614
monotone-RK <kobaya4121@gmail.com>
2018-04-07 06:50:29 +0900

    init.lua has been added

---
+++ init.lua
@@ -0,0 +1,66 @@
+local function keyCode(key, modifiers, callback)
+   modifiers = modifiers or {}
+   callback = callback or function() end
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+      callback()
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function killLine()
+   return keyCode('right', {'cmd', 'shift'}, keyCode('x', {'cmd'}))
+end
+
+-- watch & switch hotkey settings
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      -- hs.alert.show(name)
+      -- if name == "Microsoft Word" then
+      if string.find(name, "Microsoft") then
+	 enableAllHotkeys()
+      elseif name == "Thunderbird" then
+	 enableAllHotkeys()
+      else
+	 disableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+remapKey({'ctrl'}, 'p', keyCode('up'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'f', keyCode('right'))
+
+remapKey({'ctrl'}, 'a', keyCode('home'))
+remapKey({'ctrl'}, 'e', keyCode('end'))
+
+remapKey({'ctrl'}, 'h', keyCode('delete'))        -- backspace
+remapKey({'ctrl'}, 'd', keyCode('forwarddelete')) -- delete
+
+remapKey({'alt'}, 'w', keyCode('c', {'cmd'}))     -- copy
+remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))    -- cut
+remapKey({'ctrl'}, 'k', killLine())               -- kill-line
+remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))    -- paste
+remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))    -- undo
