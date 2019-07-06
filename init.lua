3d8fa4bdfcf63509f836831c9fd8a6df47998556
monotone-RK <kobaya4121@gmail.com>
2019-07-07 02:01:35 +0900

    add Emacs Mark-mode keybinding

--- init.lua
+++ init.lua
@@ -1,3 +1,109 @@
+-- Emacs Mark Mode settings begin
+
+local showInfo = false
+local function info(message)
+    if showInfo then
+        hs.alert.show(message)
+    end
+end
+-- hs.hotkey.bind({'cmd', 'shift', 'ctrl'}, 'D', function() showInfo = not(showInfo) end)
+
+local function getTableLength(t)
+    local count = 0
+    for _ in pairs(t) do count = count + 1 end
+    return count
+end
+
+local function isEqualTable(t1, t2)
+    if getTableLength(t1) ~= getTableLength(t2) then return false end
+    for k, v in pairs(t1) do
+        if t2[k] ~= v then
+            return false
+        end
+    end
+    return true
+end
+
+local function pressKey(modifiers, key)
+    modifiers = modifiers or {}
+    hs.eventtap.keyStroke(modifiers, key, 20 * 1000)
+end
+
+local function pressKeyFunc(modifiers, key)
+    return function() pressKey(modifiers, key) end
+end
+
+local function cursorWithMarkModeFunc(modifiers, key)
+   return function()
+      pressKey(modifiers, key)
+      markMode:enable()
+   end
+end
+
+SubMode = {}
+SubMode.new = function(name, commandTable, othersFunc)
+    local obj = {}
+    obj.name = name
+    obj.commandTable = commandTable
+    obj.othersFunc = othersFunc
+    obj.commandWatcher = {}
+    obj.enable = function(self)
+        info(self.name.." start")
+        self.commandWatcher:start()
+    end
+
+    obj.disable = function(self)
+        info(self.name.." end")
+        self.commandWatcher:stop()
+    end
+
+    obj.commandWatcher = hs.eventtap.new( {hs.eventtap.event.types.keyDown},
+        function(tapEvent)
+            for k,v in pairs(obj.commandTable) do
+                if v.key == hs.keycodes.map[tapEvent:getKeyCode()] and isEqualTable(v.modifiers, tapEvent:getFlags()) then
+                    info(obj.name.." end")
+                    obj.commandWatcher:stop()
+                    if v.func then
+                        v.func()
+                    end
+                    return true
+                end
+            end
+
+            if obj.othersFunc then
+                return othersFunc(tapEvent)
+            end
+        end)
+    return obj
+end
+
+markMode = SubMode.new(
+    "Mark Mode",
+    {
+        {modifiers = {ctrl = true}, key = 'space'}, -- only disables mark mode
+        {modifiers = {ctrl = true}, key = 'g'}, -- only disables mark mode
+        {modifiers = {ctrl = true}, key = 'w', func = pressKeyFunc({'cmd'}, 'x')},
+        {modifiers = {alt = true},  key = 'w', func = pressKeyFunc({'cmd'}, 'c')}, 
+        {modifiers = {ctrl = true}, key = 'p', func = cursorWithMarkModeFunc({'shift'}, 'up')},
+        {modifiers = {ctrl = true}, key = 'n', func = cursorWithMarkModeFunc({'shift'}, 'down')},
+        {modifiers = {ctrl = true}, key = 'b', func = cursorWithMarkModeFunc({'shift'}, 'left')},
+        {modifiers = {ctrl = true}, key = 'f', func = cursorWithMarkModeFunc({'shift'}, 'right')},
+        {modifiers = {ctrl = true}, key = 'a', func = cursorWithMarkModeFunc({'cmd', 'shift'}, 'left')},
+        {modifiers = {ctrl = true}, key = 'e', func = cursorWithMarkModeFunc({'cmd', 'shift'}, 'right')},
+	{modifiers = {ctrl = true}, key = 'v', func = cursorWithMarkModeFunc({'shift'}, 'pagedown')},
+	{modifiers = {alt = true},  key = 'v', func = cursorWithMarkModeFunc({'shift'}, 'pageup')}
+    },
+
+    function(tapEvent) -- force shift on
+        flags = tapEvent:getFlags()
+        flags.shift = true
+        tapEvent:setFlags(flags)
+        return false
+    end
+)
+
+-- Emacs Mark Mode settings end
+
 local function keyCode(key, modifiers, callback)
    modifiers = modifiers or {}
    callback = callback or function() end
@@ -49,6 +155,7 @@ local function handleGlobalAppEvent(name, event, app)
 	 enableAllHotkeys()
       else
 	 disableAllHotkeys()
+	 markMode:disable()
       end
    end
 end
@@ -76,3 +183,5 @@ remapKey({'ctrl'}, 'k', killLine())                     -- kill-line
 remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))          -- paste
 remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))          -- undo
 remapKey({'alt'}, '/', keyCode('z', {'cmd', 'shift'}))  -- redo
+
+remapKey({'ctrl'}, 'space', function() markMode:enable() end)
